732282040d6561a9e53ac29596c847ab4be8cff6
retrhelo <artyomliu@foxmail.com>
2022-05-03 11:22:22 +0800

    fix: fix some detailed configurations
    - Change to use Google Noto font.
    - Add exception handling logic to scripts/build.sh.
    - Fix mistakes in main.tex.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,16 @@
 
 # Build up bibliography
 echo >main.bib
-for i in bibliography/*; do
-  cat $i >main.bib
+for i in bibliography/*.bib; do
+  cat $i >>main.bib
 done
 
 # First build main.aux
-xelatex main
+xelatex main || exit -1;
 
 # Then build main.blb
-bibtex main
+bibtex main || exit -1;
 
 # Build main.pdf
-xelatex main
-xelatex main
+xelatex main || exit -1;
+xelatex main || exit -1;
